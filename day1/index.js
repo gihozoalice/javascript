1849c1d76793e9881a6e727f2b24c270e5982259
gihozo <alicegihozo13@gmail>
2023-11-22 02:07:52 -0800

    variables and operaters

---
+++ day1/index.js
@@ -0,0 +1,37 @@
+//  this is my name
+var myName = "gihozo";
+console.log(myName);
+//the decareration of valiable
+let  name= "hello";
+console.log(name)
+// the sum of two numbers
+var a = 40;
+var b = 40;
+sum = a + b;
+console.log(sum)
+// the product of two numbers
+var a = 40;
+var b = 1;
+c = a * b;
+console.log(c)
+// the substration of two numbers
+var a = 20;
+var b = 10;
+sub = a - b;
+console.log(sub)
+
+// the division of two nimbers
+var a = 30;
+var b = 2;
+b = a - b;
+console.log(b)
+//the modelus of two number 
+var a = 500;
+var b = 2;
+d = a % b;
+console.log( d)
+// the remender  of two numbers
+var a = 45;
+var b = 15;
+c = 45 % 15;
+console.log(c);
