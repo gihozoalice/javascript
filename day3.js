0488c8e3003cc6727e7f2e88e6461de78acaa85f
gihozo <alicegihozo13@gmail>
2023-11-23 08:27:51 -0800

    day3

---
+++ day3.js
@@ -0,0 +1,9 @@
+//the boolen and operators0n epual sign
+const aliceAge = 22;
+const alineAge = 22;
+console.log(aliceAge == alineAge);
+// the operatores not equal 
+ const aliceAge = 22 ;
+ const aliineAge = 23 ;
+ console.log(aliceAge != aliineAge);
+ 
